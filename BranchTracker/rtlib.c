7b80263205a956081bd9e4b2f148198b88eee8ec
Lemon-mon-ade <70093287+shang-yifei@users.noreply.github.com>
2023-12-11 21:12:39 -0500

    Add files via upload

---
+++ BranchTracker/rtlib.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void logBranchActivity(const char* str) {
+    printf("%s\n", str);
+}
---
+++ BranchTracker/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Step 1: Create and navigate to the build directory
+mkdir build
+cd build
+
+# Step 2: Run CMake and make
+cmake ..
+make
+
+# Step 3: Navigate back to the parent directory
+cd ..
+
+# Step 4: Compile rtlib.c
+cc -c rtlib.c
+
+# Step 5: Compile fileX.c with the clang plugin
+clang -fpass-plugin='build/skeleton/SkeletonPass.so' -c test1.c -g
+
+# Step 6: Link the object files and create the executable
+cc test1.o rtlib.o
+
+# Step 7: Run the executable
+./a.out
